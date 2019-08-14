87514425b8e885b558c250aa48d2ae3c4e7d7dbe
SwetaMajhi <52932227+SwetaMajhi@users.noreply.github.com>
2019-08-14 15:32:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-// Code goes here
+// Add your javascript here
 
+   $('#right-button').click(function() {
+      event.preventDefault();
+      $('#content').animate({
+        scrollLeft: "+=200px"
+      }, "slow");
+   });
+   
+     $('#left-button').click(function() {
+      event.preventDefault();
+      $('#content').animate({
+        scrollLeft: "-=200px"
+      }, "slow");
+   });
